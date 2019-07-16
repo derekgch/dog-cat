08cb7fd0fbed776d39e64d5bc47124274c088363
[Derek] <derek@derekgc.com>
2019-07-16 14:09:28 -0400

    folder in current path

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+
+current_dir = os.getcwd()
+base_dir = current_dir+ '/cats_and_dogs_filtered'
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+
+# Directory with our training cat pictures
+train_cats_dir = os.path.join(train_dir, 'cats')
+
+# Directory with our training dog pictures
+train_dogs_dir = os.path.join(train_dir, 'dogs')
+
+# Directory with our validation cat pictures
+validation_cats_dir = os.path.join(validation_dir, 'cats')
+
+# Directory with our validation dog pictures
+validation_dogs_dir = os.path.join(validation_dir, 'dogs')
+
+
+train_cat_fnames = os.listdir(train_cats_dir)
+print (train_cat_fnames[:10])
+
+train_dog_fnames = os.listdir(train_dogs_dir)
+train_dog_fnames.sort()
+print (train_dog_fnames[:10])
+
+print ( 'total training cat images:', len(os.listdir(train_cats_dir)) )
+print ( 'total training dog images:', len(os.listdir(train_dogs_dir)) )
+print ( 'total validation cat images:', len(os.listdir(validation_cats_dir)) )
+print ( 'total validation dog images:', len(os.listdir(validation_dogs_dir)) )
